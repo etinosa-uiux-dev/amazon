b968178923a131b990601bb6352dbfc3699702cc
calebetinosa <caleb20etinosa@gmail.com>
2025-05-27 12:21:29 +0100

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,6 +4,7 @@ import { formatCurrency } from './utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import { deliveryOptions } from '../data/deliveryOptions.js';
 import '../data/cart-oop.js'
+import '../data/backend-practice.js';
 
 const today = (dayjs());
 const deliveryDate = today.add (7, 'days');
